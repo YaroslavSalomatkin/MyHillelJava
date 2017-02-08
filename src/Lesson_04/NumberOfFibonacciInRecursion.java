78f6ee67c775f73a300b03889571b6338e1d972e
YAROSLAV <salomatkin@gmail.com>
2017-02-08 17:48:30 +0200

    - adding Recursion

---
+++ src/Lesson_04/NumberOfFibonacciInRecursion.java
@@ -0,0 +1,23 @@
+package Lesson_04;
+
+import java.util.Scanner;
+
+public class NumberOfFibonacciInRecursion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter index number in the sequence Fibonacci: ");
+        int index = scanner.nextInt();
+        while (index < 0) {
+            System.out.print("Incorrect! Try again: ");
+            index = scanner.nextInt();
+        }
+        System.out.print("\nNumber " + index + " in the sequence Fibonacci = " + fibonacciRecursion(index));
+    }
+
+    private static int fibonacciRecursion(int index) {
+        if (index == 1 || index == 2) {
+            return 1;
+        }
+        return fibonacciRecursion(index - 1) + fibonacciRecursion(index - 2);
+    }
+}
