84d401b56b0f316890a7a5d190c7bac80ea0bae8
YAROSLAV <salomatkin@gmail.com>
2017-02-08 16:22:10 +0200

    adding Author (test)

--- src/Lesson_01/Calculator.java
+++ src/Lesson_01/Calculator.java
@@ -1,10 +1,12 @@
 package Lesson_01;
+
 import java.util.Scanner;
 
 /**
- * Test header
+ * @author YAROSLAV
  */
 public class Calculator {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please input FIRST number: ");
