f8f46c672b509387ff4d2a876536aa67cf7ad419
YAROSLAV <salomatkin@gmail.com>
2017-03-19 22:15:32 +0200

    triangle

--- src/lesson_09/CalculationOfInkSupply/Triangle.java
+++ src/lesson_09/CalculationOfInkSupply/Triangle.java
@@ -14,7 +14,7 @@ public class Triangle extends Figure {
     @Override
     public double space() {
         // Heron's formula
-        double perimeter = (side1 + side2 + side3)/2;
-        return Math.sqrt(perimeter*(perimeter-side1)*(perimeter-side2)*(perimeter-side3));
+        double perimeter = (side1 + side2 + side3) / 2;
+        return Math.sqrt(perimeter * (perimeter - side1) * (perimeter - side2) * (perimeter - side3));
     }
 }
